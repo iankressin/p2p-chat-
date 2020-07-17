c48da79ae605a965d4c534bb900b4acc60bd8650
Ian <ianguimaraes31@gmail.com>
2020-07-17 01:31:55 -0300

    Building the user interface

---
+++ client/js/index.js
@@ -0,0 +1,32 @@
+function handleFakeClick() {
+  console.log("RUNNING FROM JS FILE");
+}
+
+const { ipcRenderer } = require("electron");
+
+ipcRenderer.on("remote::message", (event, message) => {
+  console.log(message);
+  addMessageToBoard("from-others", message.message.message);
+});
+
+function send(event) {
+  event.preventDefault();
+
+  const message = document.getElementById("message").value;
+
+  addMessageToBoard("from-me", message);
+
+  ipcRenderer.send("interface::message", { message });
+
+  document.getElementById("message").value = "";
+}
+
+function addMessageToBoard(source, message) {
+  const messageTemplate = document.getElementById(source);
+  const messageTemplateClone = messageTemplate.content.cloneNode(true);
+  const board = document.getElementById("board");
+
+  messageTemplateClone.querySelector(".message-content").innerHTML = message;
+
+  board.appendChild(messageTemplateClone);
+}
